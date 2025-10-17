e612e395f291eed185264447a66cf1feca5f7f8b
kiteCodePro <128288912+kiteCodePro@users.noreply.github.com>
2025-10-17 20:30:37 +0800

    Add files

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Profile from './profile/Profile';
+// import App from './App';
+// import MyHtml from './myhtml';
+
+// import MyHtml from './myhtml';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Profile />);
