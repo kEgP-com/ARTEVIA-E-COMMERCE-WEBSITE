9513d761434f2e2b26ee844bb0a83d24707457e8
Your Name <you@example.com>
2025-10-18 23:48:08 +0800

    Replace App component with CartPage in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './CartPage';
+import { CartPage } from './CartPage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <CartPage />
   </React.StrictMode>
 );
