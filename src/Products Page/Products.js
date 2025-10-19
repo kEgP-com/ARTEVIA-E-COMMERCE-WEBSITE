8b141ae1716644f32e5de633a004b3bd8464ef58
Rere-arch <racheljoyramos9@gmail.com>
2025-10-19 09:21:28 +0800

    changes

--- src/Products Page/Products.js
+++ src/Products Page/Products.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { LinearIndeterminate } from "./LinearIndeterminate";
-import { NavBar } from "./NavBar";
-import { Product } from "./Product";
-import { SearchBrowser } from "./SearchBrowser";
+import  LinearIndeterminate  from "./LinearIndeterminate";
+import  NavBar  from "./NavBar";
+import  Product  from "./Product";
+import  SearchBrowser from "./SearchBrowser";
 import facebook from "./facebook.png";
 import instagram from "./instagram.png";
 import qrCode from "./qr-code.png";
@@ -64,84 +64,11 @@ export const Products = () => {
           </p>
         </div>
 
-        <LinearIndeterminate
-          buildingBlocksTrackShapeClassName="linear-indeterminate-progress-indicator"
-          className="line"
-          step="zero"
-          thickness="four-dp"
-          type="flat"
-        />
-        <LinearIndeterminate
-          buildingBlocksTrackShapeClassName="linear-indeterminate-progress-indicator"
-          className="linear-indeterminate-instance"
-          step="zero"
-          thickness="four-dp"
-          type="flat"
-        />
-        <LinearIndeterminate
-          buildingBlocksTrackShapeClassName="linear-indeterminate-progress-indicator"
-          className="linear-indeterminate-progress-indicator-instance"
-          step="zero"
-          thickness="four-dp"
-          type="flat"
-        />
+        
       </footer>
 
-      <div className="text-wrapper-4">PRODUCTS/CATEGORIES</div>
-
-      <div className="group-5">
-        <div className="group-6">
-          <Product
-            className="product-instance"
-            property1="default"
-            squareWithAnImage="square-with-an-image-icon-3.png"
-          />
-          <Product
-            className="product-2"
-            property1="default"
-            squareWithAnImage="square-with-an-image-icon-4.png"
-          />
-          <Product
-            className="product-3"
-            property1="default"
-            squareWithAnImage="square-with-an-image-icon-5.png"
-          />
-        </div>
-
-        <div className="group-6">
-          <Product
-            className="product-instance"
-            property1="default"
-            squareWithAnImage="square-with-an-image-icon-6.png"
-          />
-          <Product
-            className="product-4"
-            property1="default"
-            squareWithAnImage="square-with-an-image-icon-7.png"
-          />
-          <Product
-            className="product-5"
-            property1="default"
-            squareWithAnImage="square-with-an-image-icon-8.png"
-          />
-        </div>
-      </div>
-
-      <SearchBrowser className="search-browser-product" />
-      <NavBar
-        HOMEHRBR="HOMEHRBR-logo3-1-2.png"
-        TEXTHomeClassName="nav-bar-3"
-        TEXTText="ORDERS"
-        className="nav-bar-instance"
-        icons="icons-2.svg"
-        img="nav-division-3.svg"
-        navDivision="nav-division-2.svg"
-        navDivisionClassName="nav-bar-2"
-        navDivisionClassNameOverride="nav-bar-2"
-        property1="default"
-        search="search-2.png"
-        text=""
-      />
+    
+     
     </div>
   );
 };
