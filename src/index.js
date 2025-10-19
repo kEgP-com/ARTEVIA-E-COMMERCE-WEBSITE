751d97c5c9b076f14ddfa8d1d3308d7127f9ac98
Rere-arch <racheljoyramos9@gmail.com>
2025-10-19 09:16:57 +0800

    changes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Products from '.Products Page/Products'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   
+    <Products/>
   </React.StrictMode>
 );
