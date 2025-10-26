12375bbc1db3c421dfb558857fd5d45c424d5fd4
Rere-arch <racheljoyramos9@gmail.com>
2025-10-26 20:45:37 +0800

    updated files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import ProductApp from './ProductApp';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ProductApp />
+    <App />
   </React.StrictMode>
 );
